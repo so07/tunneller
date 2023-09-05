24af9bff9569c8e029678d1494fc0872cb1a8155
Sergio Orlandini <s.orlandini@cineca.it>
2023-09-05 15:08:16 +0200

    update to release 0.1.5

--- src/tunneller/tunnel.py
+++ src/tunneller/tunnel.py
@@ -226,7 +226,7 @@ def main():
     parser.add_argument(
         "--version",
         action="version",
-        version="0.1.4",
+        version="0.1.5",
         help="print version and exit",
     )
 
